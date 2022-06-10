e3bad3c5db18aad15a35883f050ef80bb6f57a9e
Novia Phan <noviaphan@gmail.com>
2022-06-10 00:01:45 -0400

    Category and index.js updates

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,7 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
+      password: 'root',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
+
 
 module.exports = {
   Product,
